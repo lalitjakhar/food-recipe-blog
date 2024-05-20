32205ad1b166b7525929f112ff51b6d39f62f612
jakharzyneto <lalit.jakhar@zyneto.com>
2024-05-20 13:00:56 +0530

    Homepage ui CategoriesFood section

--- app/components/homePage/categoriesFood/CategoriesFood.js
+++ app/components/homePage/categoriesFood/CategoriesFood.js
@@ -1,5 +1,6 @@
 import { Container } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const CategoriesFood = () => {
@@ -7,45 +8,39 @@ const CategoriesFood = () => {
     <>
       <div className="md:py-[56px] py-[26px]" style={{ background: "#f0f0f0" }}>
         <Container>
-          <div
-            style={{
-              position: "relative",
-              width: "700px",
-              height: "700px",
-            }}
-          >
-            <Image
-              src="/assets/recipelisthome.jpg"
-              alt="Image 1"
-              layout="fill"
-              objectFit="cover"
-            />
-            <div
-              style={{
-                position: "absolute",
-                top: "50%",
-                left: "130%",
-                transform: "translate(-50%, -50%)",
-                background: "rgba(255, 255, 255)",
-                padding: "20px",
-                border: "2px solid green",
-                borderRadius: "16px 0px",
-                width: "350px",
-              }}
-            >
-              <h4
+          <div className="row flex">
+            <div className="col">
+              <div
                 style={{
-                  fontSize: "26px",
-                  color: "green",
-                  fontWeight: "600",
+                  position: "relative",
+                  width: "600px",
+                  height: "600px",
                 }}
               >
-                Vegan cocoa pudding{" "}
-              </h4>
-              <p style={{ fontSize: "12px", color: "black" }}>
-                Lorem ipsum dolor sit amet, consectetur adipicibe elit, sed do
-                eiusmod tempor inci didunt ut labore e dolore magnna ad aliquam.
-                Ut enim ad mini
+                <Image
+                  src="/assets/recipelisthome.jpg"
+                  alt="Image 1"
+                  layout="fill"
+                  objectFit="cover"
+                />
+              </div>
+            </div>
+            <div className="col flex flex-col sm:gap-[28px] gap-[8px] justify-center">
+              <h3 className="HeadingCategoriesFood">Our Food</h3>
+              <p className="ParaCategoriesFood">
+                The food at Wild Ginger spans the eastern Pacific Rim from China
+                to Indonesia, and Malaysia to Vietnam. Come take a culinary tour
+                of China and Southeast Asia — our staff is here to guide and
+                inspire you. Family style service, like in classic Asian
+                restaurants, creates an enriching experience where guests share
+                a range of tastes together.
+              </p>
+              <p className="ParaCategoriesFood">
+                We’re looking forward to seeing you again.Changing to meet
+                customer needs throughout the day, Wild Ginger is as much a
+                place to meet with co-workers over a working lunch, as it is to
+                enjoy bespoke cocktails after work, or relax with friends and
+                family over dinner.
               </p>
             </div>
           </div>
